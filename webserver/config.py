6f0add8b16407e70bc3341cd68812110bba0d8a7
RobertoMD <robertomd@gmail.com>
2014-05-14 20:40:43 +0200

    new file:   config.py 	new file:   static/css/bootstrap.css.original 	new file:   static/css/bootstrap.min.css.original 	new file:   static/custom.css 	new file:   static/fonts/FontAwesome.otf 	new file:   static/fonts/fontawesome-webfont.eot 	new file:   static/fonts/fontawesome-webfont.svg 	new file:   static/fonts/fontawesome-webfont.ttf 	new file:   static/fonts/fontawesome-webfont.woff 	new file:   static/js/my.js

---
+++ webserver/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+LABEL_TEMP1 = u'Salón'
+LABEL_TEMP2 = u'Salón2'
+# Relay list
+RELAYS={'R1LINE'}
+# Relay status
+RSTATUS={'ON','OFF'}
+# Line for first relay
+R1LINE = 11
+R1NAME = u'Salón'
