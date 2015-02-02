d02a03b6f4d3a59091bc7473d7917c70cc3401e8
RobertoMD <robertomd@gmail.com>
2015-02-02 19:25:27 +0100

    modified:   cloud/ts.py 	modified:   controller/radio.py 	cloud/GPIO.py 	cloud/nrf24.py 	cloud/radio.py

--- cloud/ts.py
+++ cloud/ts.py
@@ -4,14 +4,19 @@ import datetime
 import time
 import os
 import util
+import radio
  
-def sendData():
+def sendData(radio):
 	try:
 		temp1=util.getTemperature(0)
 		temp2=util.getTemperature(1)
 		temp3=util.getTemperature(2)
 		pressure=util.getPressure()
-		params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'key':'M3WA24ZHF4KDYM56'})
+		temp4=radio.getTemp()
+		if temp4 is None:
+			params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'key':'M3WA24ZHF4KDYM56'})
+		else:
+			params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'field5': temp4, 'key':'M3WA24ZHF4KDYM56'})
 		headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 		conn = httplib.HTTPConnection("api.thingspeak.com:80")
 		conn.request("POST", "/update", params, headers)
@@ -25,15 +30,21 @@ def sendData():
 #sleep for 300 seconds (api limit of 15 secs)
 if __name__ == "__main__":
 	# start checks - print data at startup
+	radio=radio.radio()
 	temp1=util.getTemperature(0)
 	temp2=util.getTemperature(1)
 	temp3=util.getTemperature(2)
 	pressure=util.getPressure()
-	params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'key':'M3WA24ZHF4KDYM56'})
+	temp4=radio.getTemp()
 	print temp1," degrees"
 	print temp2," degrees"
 	print temp3," degrees"
 	print pressure, " mbar"
+	if radio is None:
+		params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'key':'M3WA24ZHF4KDYM56'})
+	else:
+		params = urllib.urlencode({'field1': temp1, 'field2': temp2, 'field3': pressure, 'field4': temp3, 'field5': temp4, 'key':'M3WA24ZHF4KDYM56'})
+		print temp4," degrees"
 	print params
 	# end checks
 	now=datetime.datetime.now()
@@ -44,5 +55,5 @@ if __name__ == "__main__":
 	print "Waiting",r,"seconds for synchronization"
 	time.sleep(r)
 	while True:
-		sendData()
+		sendData(radio)
 		time.sleep(600)
